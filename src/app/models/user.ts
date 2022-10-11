524face2a2df42f71b39d009e02c6949eb057aa7
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-10-11 20:47:44 -0300

    feat: add classe de User

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,4 +1,8 @@
-export interface User {
+export interface IUser {
     email: string,
     password: string
+}
+
+export class User implements IUser {
+    constructor(public email : string, public password : string){}
 }
