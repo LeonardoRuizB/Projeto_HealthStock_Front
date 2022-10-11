340b940d4ec06c017ff4893937c1bcaee5b443a1
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-10-11 20:48:49 -0300

    feat: add login para formulário

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formCliente : FormGroup;
+  message : string | undefined;
 
-  constructor() { }
+  constructor(private authService : AuthService) {
+    let formBuilder = new FormBuilder(), user = new User('', '');
+    
+    this.formCliente = formBuilder.group({
+      email: [ user.email ],
+      password: [ user.password ]
+    });
+  }
 
   ngOnInit(): void {
+    //this.onSubmit();
+  }
+  
+  onSubmit(): void {
+    let messageLogin = this.authService.doLogin(this.formCliente.value);
+  
+    messageLogin.subscribe({
+      error: responseError => { 
+        this.message = responseError.error.error;
+      },
+    });
   }
 
 }
