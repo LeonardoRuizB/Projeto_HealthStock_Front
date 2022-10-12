dd05c2a08a7b2ff6f2931c075e5aa000f070879a
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-10-11 21:06:43 -0300

    feat: add mensagem quando sistema não estiver funcionando

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from 'src/app/service/auth.service';
 import { User } from 'src/app/models/user';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -29,8 +30,14 @@ export class LoginComponent implements OnInit {
     let messageLogin = this.authService.doLogin(this.formCliente.value);
   
     messageLogin.subscribe({
-      error: responseError => { 
-        this.message = responseError.error.error;
+      error: responseError => {
+        if(responseError.status == 0){
+          this.message = "O Serviço de Login não está funcionando corretamente!";
+          if(!environment.production)
+            this.message += "Você tem que dar um npm start no serviço de Login. Não esquece de clonar o serviço. https://github.com/JoaoGabrielOliveira/healthstock-login";
+        }
+        else
+          this.message = responseError.error.error;
       },
     });
   }
