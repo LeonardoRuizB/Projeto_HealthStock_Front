d96c83592a6e9569043d6f869a14b459e2fab86c
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-10-26 16:33:04 -0300

    feat: criando modelos para comprador e fornecedor

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,6 +3,10 @@ export interface IUser {
     password: string
 }
 
+export interface IBaseUser {
+    companyName : string;
+}
+
 export class User implements IUser {
     constructor(public email : string, public password : string){}
 }
