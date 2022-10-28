ffcfd4efde674696da84d203ed2fee8a97b173a1
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-10-28 11:32:06 -0300

    feat: alterando imagems e texto dos slides

--- src/app/models/slide.ts
+++ src/app/models/slide.ts
@@ -3,6 +3,7 @@ export interface ISlide {
     date: string;
     text: string;
     img: string;
+    buttonMessage: string;
 }
 
 export default class Slide implements ISlide {
@@ -10,19 +11,21 @@ export default class Slide implements ISlide {
     date: string;
     text: string;
     img: string;
+    buttonMessage: string;
   
-    constructor(title:string, date:string, text:string, img:string){
+    constructor(title:string, date:string, text:string, img:string, buttonMessage : string = "Saiba mais!"){
       this.title = title;
       this.date = date;
       this.text = text;
       this.img = img;
+      this.buttonMessage = buttonMessage;
     }
 
     static makeSlides(){
       return [
-        new Slide("HealthStock", 'Uma nova forma manter o estoque', "Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim.Quem num gosta di mé, boa gentis num é.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.", "backgrounds/background1.jpg"),
-        new Slide("Family House", '2025', "Quem manda na minha terra sou euzis!Suco de cevadiss deixa as pessoas mais interessantis.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.", "backgrounds/background2.jpg"),
-        new Slide("Modern Architecture", '2026', "Si num tem leite então bota uma pinga aí cumpadi!Viva Forevis aptent taciti sociosqu ad litora torquent.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!", "backgrounds/background3.jpg")
+        new Slide("HealthStock", 'Uma nova forma manter o estoque', "Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim.Quem num gosta di mé, boa gentis num é.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.", "backgrounds/background1.jpg", "Venha conhecer a plataforma!"),
+        new Slide("Marketplace", 'Uma solução pensada para você', "Uma platforma segura para compra de itens hospitaláres.", "backgrounds/background2.jpg", "Saiba mais como comprar nossos produtos!"),
+        new Slide("Venda seus produtos aqui", '2026', "Com um grande número de compradores, venha fazer parte da história e venda seus produtos aqui. Conheça nossos estudos de caso e descobra como podemos aumentar suas vendas!", "backgrounds/background3.jpg", "Saiba mais sobre vendas")
       ];
     }
 }
