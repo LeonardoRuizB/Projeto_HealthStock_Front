9c03e9bbcc96a43c90e61989d197bac9851a42ee
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-03 21:12:38 -0300

    refactor: melhorando tela de produtos para pegar produtos de um array

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  products : any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products = [
+      {name:"Caneta Azul", img: ""},
+      {name:"Caneta Vermelha", img: ""},
+      {name:"Cadeira", img: ""}
+    ];
   }
 
 }
