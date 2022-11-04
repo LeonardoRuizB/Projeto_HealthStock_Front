6accf26cb4f1b25421ae550782d4d163b0bd18cf
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-03 23:37:38 -0300

    feat: criando esquema para criação de paginas

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from 'src/app/service/product/product.service';
+import {  firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -8,16 +10,40 @@ import { ProductService } from 'src/app/service/product/product.service';
 })
 export class ProductsComponent implements OnInit {
 
+  totalProdutcs : number = 0;
+  pageNumber : number = 1;
+  totalPages : number = 0;
+  productsByPage : number = 2;
+
   products : any[] = [];
 
-  constructor(private productService : ProductService) { }
+  constructor(private productService : ProductService, private activatedRoute : ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.productService.getProdutos().subscribe({
-      next: products => {
-        this.products = products;
-      }
-    });
+    this.initPagination();
+  }
+
+  async initPagination(){
+    
+    this.pageNumber = await this.getPageNumber(); 
+    
+    let total = await firstValueFrom(this.productService.getTotalProdutos());
+    this.totalProdutcs = total.total;
+
+    this.totalPages = this.totalProdutcs / this.productsByPage;
+    let offset = this.productsByPage * (this.pageNumber - 1);
+    console.log(offset);
+
+    this.products = await firstValueFrom(this.productService.getProdutos(this.productsByPage, offset));
+  }
+
+  updatePage(){
+    this.initPagination();
+  }
+
+  async getPageNumber(){
+    let params = await firstValueFrom(this.activatedRoute.queryParams);
+    return params['page'] ?? 1;
   }
 
 }
