37a53ca33a7ac2a8120c68845bd0994fa4dda703
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-06 20:22:07 -0300

    feat: implementando pre-visualizacao de imagens de produto

--- src/app/pages/cadastro-produto/cadastro-produto.component.ts
+++ src/app/pages/cadastro-produto/cadastro-produto.component.ts
@@ -12,36 +12,71 @@ import { NotificationService } from 'src/app/service/notification/notification.s
 })
 export class CadastroProdutoComponent implements OnInit {
 
-  formProduto : FormGroup;
-  categories : any = []
+  formProduto: FormGroup;
+  categories: any = [];
+  photos : any[] = [];
 
-  constructor(private productService:ProductService, private categoryService:CategoryService,
-    private notificationService: NotificationService ) {let formBuilder = new FormBuilder();
-    
+  constructor(private productService: ProductService, private categoryService: CategoryService,
+    private notificationService: NotificationService) {
+      let formBuilder = new FormBuilder();
+
     this.formProduto = formBuilder.group({
-      name: [ '' ],
-      description: [ '' ], 
-      categoryId: [ '' ]
-    }); }
+      name: [''],
+      description: [''],
+      categoryId: ['']
+    });
+  }
 
   ngOnInit(): void {
     this.categoryService.getCategories()
-    .subscribe({
-      next : response => {
-        this.categories = response;
-      }
-    })
+      .subscribe({
+        next: response => {
+          this.categories = response;
+        }
+      })
   }
-onSubmit(){
-  this.notificationService.showMessage('Produto cadastrado com sucesso!');
-  /*this.productService.createProduto(this.formProduto.value).subscribe({
-    next: response => {
-      this.notificationService.showMessage('Produto cadastrado com sucesso!');
-    }
-  });
-  */
- this.formProduto.reset();
+  onSubmit() {
+    this.notificationService.showMessage('Produto cadastrado com sucesso!');
+    /*this.productService.createProduto(this.formProduto.value).subscribe({
+      next: response => {
+        this.notificationService.showMessage('Produto cadastrado com sucesso!');
+      }
+    });
+    */
+    this.formProduto.reset();
 
-  console.log(this.formProduto.value)
-}
+    console.log(this.formProduto.value)
+  }
+
+  onUpload(){
+
+  }
+
+  onChangeFileInput(event: Event){
+    const input = event.target as HTMLInputElement;
+    let tempFiles = [].slice.call(input.files);
+    let photoFiles : any[] = [];
+    
+    const reader = new FileReader();
+    tempFiles.forEach((value : any, index) => {
+      let photoFile = {name:'', data:{}};
+
+      reader.onload = () => {
+        photoFile.name = value.name;
+        photoFile.data = reader.result as string;
+      }
+
+      photoFiles.push(photoFile);
+
+      reader.readAsDataURL(value);
+    });
+
+    this.photos.push(...photoFiles);
+
+    this.updatePreview();
+  }
+
+  updatePreview(){
+    console.log(this.photos);
+  }
 }
