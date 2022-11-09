34d0c67af6983167243512af719f2d8b09d333fc
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-09 17:17:14 -0300

    feat: criando pagina de resgistro de produtos de fornecedor

--- src/app/pages/supplier/components/register/register.component.ts
+++ src/app/pages/supplier/components/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ICategory } from 'src/app/models/category';
+import { IPackageType } from 'src/app/models/packageType';
+import { IProduct } from 'src/app/models/product';
+import { PackageTypeService } from 'src/app/service/packagetype/packagetype.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  formCatalogue : FormGroup;
+  selectedProduct : any;
+  photos : any[] = [];
+  packeges : IPackageType[] = [];
 
-  constructor() { }
+
+  constructor(private packageTypeService : PackageTypeService) {
+    this.formCatalogue = new FormBuilder().group({
+      productId:[ 0],
+      name: [''],
+      description: [''],
+      packageTypeId: [ 0]
+    });
+
+    packageTypeService.getPackageType().subscribe({
+      next: packageTypes => {
+        this.packeges = packageTypes;
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.selectedProduct = {id: 1, name: 'Caneta'};
+  }
+
+  onSubmit(){
+
+    console.table(this.formCatalogue.value);
+  }
+
+  onChangeImage($event:Event){
+    
+  }
+
+  outDescription(event : Event){
+    let target = event.target as HTMLTextAreaElement;
+    target.value = target.value.trim()
+  }
+
+  selectProduct(){
+    this.setProductId();  
+  }
+
+  setProductId(){
+    this.formCatalogue.get('productId')?.setValue(this.selectedProduct.id);
   }
 
 }
