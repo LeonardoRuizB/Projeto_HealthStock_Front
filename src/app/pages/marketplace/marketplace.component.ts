6d509157ebafb664600adcbf6e49d8ffe23942e1
Gustavo Meneses <gustavo.jesus10@hotmail.com>
2022-11-08 23:12:37 -0300

    feat:alterando navbar e adicionando campo de pesquisa

--- src/app/pages/marketplace/marketplace.component.ts
+++ src/app/pages/marketplace/marketplace.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MarketplaceService } from 'src/app/service/marketplace/marketplace.service';
 
 @Component({
@@ -7,16 +8,38 @@ import { MarketplaceService } from 'src/app/service/marketplace/marketplace.serv
   styleUrls: ['./marketplace.component.scss']
 })
 export class MarketplaceComponent implements OnInit {
+  
   products : any[] = [];
-  constructor(private marketplace : MarketplaceService) { }
+  filters : FormGroup;
+  loadingSearching : boolean = false;
+  timer : any;
+
+  constructor(private marketplaceService : MarketplaceService) {
+    let fb = new FormBuilder();
+      this.filters = fb.group({
+        search: [ '' ]
+      });
+   }
 
   ngOnInit(): void {
-    this.marketplace.getProdutos().subscribe({
+    this.marketplaceService.getProdutos().subscribe({
       next: products => {
         this.products = products;
       console.log(this.products)
       }
     })
   }
+  changeSearch(){
+    this.loadingSearching = true;
+    clearTimeout(this.timer);
+    this.timer = setTimeout(
+      () => this.marketplaceService.getProdutos().subscribe({
+        next: marketplaceResponse => {
+          // this.router.navigate(['produtos'],{queryParams: {page: this.paginationService.pageNumber, search: this.filters.value.search}});
+          this.products = marketplaceResponse;
+          // this.paginationService.setTotalItems(productsResponse[1]);
+          this.loadingSearching = false;
+        }}),500);
+  } 
+ }
 
-}
