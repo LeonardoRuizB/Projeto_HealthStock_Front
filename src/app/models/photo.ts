325096e3087741bf3991b6a68b8f837fcad878e7
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-10 02:43:45 -0300

    feat: add classe photo

--- src/app/models/photo.ts
+++ src/app/models/photo.ts
@@ -3,3 +3,19 @@ export interface IPhoto {
     data : any;
     mimeType : string;
 }
+
+export class Photo implements IPhoto {
+    title: string;
+    data: any;
+    mimeType: string;
+    type : string;
+
+    constructor(body : any){
+        this.title = body.title;
+        this.data = body.data;
+        this.mimeType = body.mimeType;
+        this.type = this.mimeType.split('/')[1];
+
+    }
+
+}
--- src/app/models/supplierCatalogue.ts
+++ src/app/models/supplierCatalogue.ts
@@ -1,6 +1,9 @@
+import { Photo } from "./photo";
+
 export interface ISupplierCatogue {
     id: number;
     name: string;
     description: string;
     supplierId: number;
+    photo: Photo
 }
