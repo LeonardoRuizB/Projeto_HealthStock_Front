c914ff3b89ccf861b07b8269935a611c909a9ef3
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-10 11:29:35 -0300

    feat: cadastro de meus produtos com notificacao

--- src/app/pages/supplier/components/register/register.component.ts
+++ src/app/pages/supplier/components/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ICategory } from 'src/app/models/category';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { IPackageType } from 'src/app/models/packageType';
 import { IPhoto } from 'src/app/models/photo';
-import { IProduct } from 'src/app/models/product';
+import { AuthService } from 'src/app/service/auth/auth.service';
 import { ModalService } from 'src/app/service/bulma/modal/modal.service';
+import { NotificationService } from 'src/app/service/bulma/notification/notification.service';
 import { PackageTypeService } from 'src/app/service/packagetype/packagetype.service';
 import { ProductService } from 'src/app/service/product/product.service';
+import { SupplierCatalogService } from 'src/app/service/supplierCatalog/supplier-catalog.service';
 
 @Component({
   selector: 'app-register',
@@ -22,12 +23,17 @@ export class RegisterComponent implements OnInit {
   products : any[] = []
 
 
-  constructor(private packageTypeService : PackageTypeService, public modalService : ModalService, private productsService : ProductService) {
+  constructor(private packageTypeService : PackageTypeService, public modalService : ModalService,
+    private productsService : ProductService, private catalogService : SupplierCatalogService,
+    private notificationService : NotificationService, private authService : AuthService,) {
+      this.authService.needBeAuth();
+
     this.formCatalogue = new FormBuilder().group({
-      productId:[ 0],
-      name: [''],
-      description: [''],
-      packageTypeId: [ 0]
+      productId: 0,
+      name: '',
+      description: '',
+      packageTypeId: 0,
+      supplierId: this.authService.getUserData().id
     });
 
     packageTypeService.getPackageType().subscribe({
@@ -44,12 +50,36 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.authService.needBeSupplier();
+
     this.modalService.findAndSetModal('products-modal');
   }
 
   onSubmit(){
     this.setProductId();
-    console.table(this.formCatalogue.value);
+  
+    this.catalogService.createProduct(this.formCatalogue.value).subscribe({
+      next: response => {
+        if(this.photos)
+          this.catalogService.uploadPhotos(response.id, this.photos).subscribe({
+            next: response => {
+              this.notificationService.showMessage('Produto cadastrado com sucesso!');
+            },
+            error: errorResponse => {
+              this.notificationService.showMessage('Produto cadastrado com sucesso!');
+              this.notificationService.showMessage('Erro ao cadastrar foto!');
+            }
+          });
+        else 
+          this.notificationService.showMessage('Produto cadastrado com sucesso!');
+
+        this.formCatalogue.reset();
+        this.photos = [];
+      },
+      error: errorResponse => {
+        this.notificationService.showMessage("Erro ao cadastrar produto");
+      }
+    });
   }
 
   onChangeImage(event:Event){
