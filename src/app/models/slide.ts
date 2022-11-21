e9bc44ea47d5785aa7acbadecf98887284be48a6
LeonardoRuizB <leobottura12@gmail.com>
2022-11-21 01:00:57 -0300

    feat: alterando imagens do carrossel

--- src/app/models/slide.ts
+++ src/app/models/slide.ts
@@ -12,7 +12,7 @@ export default class Slide implements ISlide {
     text: string;
     img: string;
     buttonMessage: string;
-  
+
     constructor(title:string, date:string, text:string, img:string, buttonMessage : string = "Saiba mais!"){
       this.title = title;
       this.date = date;
@@ -23,8 +23,8 @@ export default class Slide implements ISlide {
 
     static makeSlides(){
       return [
-        new Slide(" 3 Motivos para utilizar a HealthStock ", '', "Nossa empresa é altamente conceituada, e estamos a cada dia desenvolvendo nossos serviços. ", "backgrounds/background1.jpg", "Venha conhecer a plataforma!"),
-        new Slide(" Agilidade", ' 1 - Motivo', " Nossos fornecedores possuem uma rede de entregas, oferecendo um prazo de entrega conveniente. ", "backgrounds/background1.jpg", "Venha conhecer a plataforma!"),
+        new Slide(" 3 Motivos para utilizar a HealthStock ", '', "Nossa empresa é altamente conceituada, e estamos a cada dia desenvolvendo nossos serviços. ", "backgrounds/background4.jpg", "Venha conhecer a plataforma!"),
+        new Slide(" Agilidade", ' 1 - Motivo', " Nossos fornecedores possuem uma rede de entregas, oferecendo um prazo de entrega conveniente. ", "backgrounds/background5.jpg", "Venha conhecer a plataforma!"),
         new Slide(" Diversidade", '2 - Motivo ', " Nossa plataforma possui a mais variada gama de produtos, visando oferecer a maior variedade de opções para nossos consumidores e clientes.", "backgrounds/background2.jpg", "Saiba mais como comprar nossos produtos!"),
         new Slide(" Confiabilidade", '3 - Motivo', "Uma plataforma segura para comprar seus produtos, seja para uso pessoal, ou para fornecimento da sua empresa. ", "backgrounds/background3.jpg", "Saiba mais sobre vendas")
       ];
