c8ebdd4041f032142baaaadbe941c6a93c67a169
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-20 22:51:48 -0300

    feat: add imagem dentro de detalhes de produto

--- src/app/pages/marketplace-product/marketplace-product.component.ts
+++ src/app/pages/marketplace-product/marketplace-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
+import { Photo } from 'src/app/models/photo';
 import { ISupplierCatogue } from 'src/app/models/supplierCatalogue';
 import { MarketplaceService } from 'src/app/service/models/marketplace/marketplace.service';
 
@@ -13,6 +14,7 @@ import { MarketplaceService } from 'src/app/service/models/marketplace/marketpla
 export class MarketplaceProductComponent implements OnInit {
 
   product : ISupplierCatogue | undefined;
+  selectedPhoto : Photo | undefined;
   constructor(private marketplaceService:MarketplaceService, private router:Router,
     private route : ActivatedRoute, private title : Title) {}
 
@@ -25,6 +27,7 @@ export class MarketplaceProductComponent implements OnInit {
       this.marketplaceService.getProduto(searchText).subscribe({
         next:product => {
           this.product = product;
+          this.selectedPhoto = this.product.photos[0];
         },
         error:error => this.router.navigate(['marketplace'])
       });
